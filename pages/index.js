dc97af46e603d62a274809552e73236b2455f3db
irreverentsimplicity@github.com <irreverentsimplicity@proton.me>
2023-09-23 00:52:33 -0400

    calling function signatures and evaluating StartGame correctly

--- pages/index.js
+++ pages/index.js
@@ -10,19 +10,15 @@ export default function Home() {
   useEffect( () => {
     getAnOutput()
   }, []);
-  const provider = new GnoJSONRPCProvider('http://test3.gno.land:36657');
-  //const provider = new GnoJSONRPCProvider('https://staging.gno.land:36657');
-  //const provider = new GnoJSONRPCProvider('http://localhost:26657');
+  const provider = new GnoJSONRPCProvider('http://localhost:26657');
   // provider is created
   const getAnOutput = async () => {
-    console.log("provider " + JSON.stringify(provider, null, 2));
-    const response = await provider.getRenderOutput('gno.land/r/demo/foo20', '');
-    //const response = await provider.getFunctionSignatures('gno.land/r/demo/foo721');
-    //const response = await provider.evaluateExpression('gno.land/r/demo/foo20', 'TotalSupply()')
-    //const response1 = await provider.evaluateExpression('gno.land/r/demo/foo721', 'init()')
-    //const response = await provider.evaluateExpression('gno.land/r/demo/foo721', 'BalanceOf("g1var589z07ppjsjd24ukm4uguzwdt0tw7g47cgm")')
     
-    console.log("response " + response);
+    const signatures = await provider.getFunctionSignatures('gno.land/r/demo/flippando', '');
+    console.log("signatures ", signatures);
+
+    const response = await provider.evaluateExpression('gno.land/r/demo/flippando', 'StartGame("owner", "gameId", "gameTyoe", 4)');
+    console.log("response ", response);
     return response;
   }  
 
