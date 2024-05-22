b9c00bd5a274bd625cfd1b8bccb5bd117b3ee6b7
irreverentsimplicity@github.com <irreverentsimplicity@proton.me>
2024-05-22 05:02:16 -0400

    update roadmap

--- pages/roadmap.js
+++ pages/roadmap.js
@@ -61,7 +61,7 @@ export default function Tutorial() {
         </Box>
         <Box className="justify-start" borderBottom="1px solid gray" mt={8}>
         
-        <Text fontSize="xl" fontWeight="bold" textAlign="left" mb={4} mr={4}>Land of the Flips (beta, testnet) - target: March 2024</Text>
+        <Text fontSize="xl" fontWeight="bold" textAlign="left" mb={4} mr={4}>✅ Land of the Flips (beta, testnet) - target: March 2024</Text>
         <Text fontSize="lg" textAlign="left" mt={4} mb={4}>
           - adjust playground dynamically to account for the current number of available basic NFTs
           </Text> 
@@ -69,10 +69,7 @@ export default function Tutorial() {
           - full game management (recover and replay interrupted games)
           </Text>
           <Text fontSize="lg" textAlign="left" mt={4} mb={4}>
-          - VFR integration
-          </Text>
-          <Text fontSize="lg" textAlign="left" mt={4} mb={4}>
-          - Adena integration and faucet improvements
+          - faucet improvements
           </Text>
           <Text fontSize="lg" textAlign="left" mt={4} mb={4}>
           - Gnoswap integration for the FLIP fungible token
@@ -83,7 +80,23 @@ export default function Tutorial() {
         </Box>
 
         <Box className="justify-start" borderBottom="1px solid gray" mt={8}>
-        <Text fontSize="xl" fontWeight="bold" textAlign="left" mb={4} mr={4}>Awakening (production, mainnet) - target: June 2024</Text>
+        
+        <Text fontSize="xl" fontWeight="bold" textAlign="left" mb={4} mr={4}>⏳ The airdrop (beta, testnet) - target: June 2024</Text>
+        <Text fontSize="lg" textAlign="left" mt={4} mb={4}>
+          - stress test for a 10,000 basic NFTs airdrop
+          </Text> 
+          <Text fontSize="lg" textAlign="left" mt={4} mb={4}>
+          - test liquidity bootstrapping for the fungible FLIP token
+          </Text>
+          <Text fontSize="lg" textAlign="left" mt={4} mb={4}>
+          - faucet hub listing
+          </Text>
+        </Box>
+
+        
+
+        <Box className="justify-start" borderBottom="1px solid gray" mt={8}>
+        <Text fontSize="xl" fontWeight="bold" textAlign="left" mb={4} mr={4}>⏳ Awakening (production, mainnet) - target: July 2024</Text>
         <Text fontSize="lg" textAlign="left" mt={4} mb={4}>
           - feature freeze
         </Text>
@@ -92,7 +105,7 @@ export default function Tutorial() {
         </Text>
       </Box>
       <Box className="justify-start" borderBottom="1px solid gray" mt={8}>  
-        <Text fontSize="xl" fontWeight="bold" textAlign="left" mb={4} mr={4}>The Flip Connection (production, mainnet, cross-chain integration) - target: September 2024</Text>
+        <Text fontSize="xl" fontWeight="bold" textAlign="left" mb={4} mr={4}>⏳ The Flip Connection (production, mainnet, cross-chain integration) - target: October 2024</Text>
         <Text fontSize="lg" textAlign="left" mt={4} mb={4}>
           - transfer / bridge NFTs from Gno to Flippando deployments on EVM chains
           </Text>  
