f1dd952a09268ebeadb93a47f9fbb8c6713481a6
irreverentsimplicity@github.com <irreverentsimplicity@proton.me>
2024-06-20 03:44:33 -0400

    cleanup

--- components/Wallet.js
+++ components/Wallet.js
@@ -35,6 +35,7 @@ const Wallet = ({ userBalances, userGnotBalances }) => {
     };
 
     const showLocalOption = process.env.NEXT_PUBLIC_SHOW_LOCAL_OPTION === 'true';
+    console.log('NEXT_PUBLIC_SHOW_LOCAL_OPTION:', process.env.NEXT_PUBLIC_SHOW_LOCAL_OPTION);
 
     return (
       <div>
