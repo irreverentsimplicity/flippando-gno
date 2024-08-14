8df138bac287258d38acfc72a4df1c42a030f112
irreverentsimplicity@github.com <irreverentsimplicity@proton.me>
2024-08-14 07:02:31 -0400

    test4 deployment targets

--- util/config.ts
+++ util/config.ts
@@ -2,6 +2,6 @@ export default {
   GNO_WS_URL: "http://localhost:26657/websocket",
   GNO_JSONRPC_URL: process.env.NEXT_PUBLIC_RPC_URL,
   //GNO_JSONRPC_URL: "http://localhost:36657",  // gno dev
-  GNO_FLIPPANDO_REALM: "gno.land/r/demo/flippando",
+  GNO_FLIPPANDO_REALM: process.env.NEXT_PUBLIC_FLIPPANDO_REALM,
   FAUCET_URL: process.env.NEXT_PUBLIC_FAUCET_URL,
 };
